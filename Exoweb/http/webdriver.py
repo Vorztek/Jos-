dcf5eb2fab7d58e3f2fb9a5c45e50b2e5f4f97c8
Vorztek <vorztek@gmail.com>
2021-09-14 19:10:04 +0200

    update

---
+++ Exoweb/http/webdriver.py
@@ -0,0 +1,46 @@
+from bs4 import BeautifulSoup
+import requests
+from pandas import pandas as pd
+import gspread
+from df2gspread import df2gspread as d2g
+from oauth2client.service_account import ServiceAccountCredentials
+
+url = ("https://www.worldometers.info/coronavirus/countries-where-coronavirus-has-spread/")
+response = requests.get(url)
+
+content = response.content
+
+soup = BeautifulSoup(content,"html.parser")
+
+country = []
+cases = []
+death = []
+continent = []
+for i in soup.find_all("tr"):
+    name = list(i)
+    country.append(name[1].text)
+    cases.append(name[3].text)
+    death.append(name[5].text)
+    continent.append(name[7].text)
+
+
+df = pd.DataFrame({
+    "country" : country,
+    "cases" : cases,
+    "death" : death,
+    "continent" : continent,
+})
+
+scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+
+credentials = ServiceAccountCredentials.from_json_keyfile_name(r"C:\Users\vorzt\Documents\python\Exoweb\http\covid-326012-a9cd67e43392.json", scope)
+
+gc = gspread.authorize(credentials)
+
+spreadsheet_key = "1hZ-1AZGsEOt52G_Xt5BZMsLi7eMetDhdTjBCkbWU3Go"
+wks_name = "coco"
+
+d2g.upload(df, spreadsheet_key, wks_name, credentials=credentials, row_names=False, col_names=False)
+
+
+
